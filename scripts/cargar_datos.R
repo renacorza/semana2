fc65774d105266b0fd6bb443b63cb6e9eb33ef68
renacorza <renacorza@gmail.com>
2026-08-18 11:30:56 -0400

    Update cargar_datos.R

--- scripts/cargar_datos.R
+++ scripts/cargar_datos.R
@@ -1,5 +1,11 @@
-install.packages("tidyverse")
+#Actividad en clases
+#18 de agosto
 
 
-library(readr)
-casen2 <- read_csv("data/raw/casen_sucio.csv")
+
+install.packages("tidyverse") #instalar paquete tidyverse
+
+#cargar la base de datos
+
+library(readr) #carga el paquete para importar archivos CSV
+casen2 <- read_csv("data/raw/casen_sucio.csv") #carga la base de datos de CASEN
